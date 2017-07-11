71d2ceb1292b158228199c9cf68900161936603c
Daniel Henao <danielhenao00@hotmail.com>
2017-07-11 15:02:00 -0500

    Iniciamos Commit

---
+++ inc/footer.php
@@ -0,0 +1,97 @@
+<?php
+?>
+    <section id="bottom" class="wet-asphalt">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <h4>Acerca de PlayChess</h4>
+                    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante.</p>
+                    <p>Pellentesque habitant morbi tristique senectus.</p>
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>PlayChess</h4>
+                    <div>
+                        <ul class="arrow">
+                            <li><a href="#">Preguntas Frecuentes</a></li>
+                            <li><a href="#">Conatcto</a></li>
+                            <li><a href="#">Política de Privacidad</a></li>
+                            <li><a href="#">Términos de Uso</a></li>
+                        </ul>
+                    </div>
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Últimos Jugadores Registrados</h4>
+                    <div>
+                        <div class="media">
+                            <div class="pull-left">
+                                <img src="images/blog/thumb1.jpg" alt="">
+                            </div>
+                            <div class="media-body">
+                                <span class="media-heading"><a href="#">Camilo Vargas</a></span>
+                                <small class="muted">Fecha: 30/05/2015</small>
+                            </div>
+                        </div>
+                        <div class="media">
+                            <div class="pull-left">
+                                <img src="images/blog/thumb2.jpg" alt="">
+                            </div>
+                            <div class="media-body">
+                                <span class="media-heading"><a href="#">Santiago Arias</a></span>
+                                <small class="muted">Fecha: 30/05/2015</small>
+                            </div>
+                        </div>
+                        <div class="media">
+                            <div class="pull-left">
+                                <img src="images/blog/thumb3.jpg" alt="">
+                            </div>
+                            <div class="media-body">
+                                <span class="media-heading"><a href="#">Radamel Falcao</a></span>
+                                <small class="muted">Fecha: 30/05/2015</small>
+                            </div>
+                        </div>
+                    </div>  
+                </div><!--/.col-md-3-->
+
+                <div class="col-md-3 col-sm-6">
+                    <h4>Redes Sociales</h4>
+					
+                    <a href="">Twitter</a><br />
+                    <a href="">Facebook</a><br />
+                    <a href="">Instagram</a><br />
+                    <a href="">YouTube</a>
+					
+					<br /><br />
+                    <h4>Newsletter</h4>
+                    <form role="form">
+                        <div class="input-group">
+                            <input type="text" class="form-control" autocomplete="off" placeholder="Ingresa tu Email">
+                            <span class="input-group-btn">
+                                <button class="btn btn-danger" type="button">Go!</button>
+                            </span>
+                        </div>
+                    </form>
+                </div> <!--/.col-md-3-->
+            </div>
+        </div>
+    </section><!--/#bottom-->
+
+    <footer id="footer" class="wet-asphalt">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                   &copy; Todos los derechos reservados - 2015 PlayChess desarrollado por <a href="">MEDIEVEL</a>
+                </div>
+                <div class="col-sm-6">
+                    <ul class="pull-right">
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Ver Planes</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Contacto</a></li>
+                        <li><a id="gototop" class="gototop" href="#"><i class="icon-chevron-up"></i></a></li><!--#gototop-->
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </footer><!--/#footer-->
---
+++ inc/menuNav.php
@@ -0,0 +1,11 @@
+<div class="collapse navbar-collapse">
+	<ul class="nav navbar-nav navbar-right">
+		<li><a href="index.php">Home</a></li>
+		<li><a href="pricing.php">Ver Planes</a></li>
+		<li><a href="faq.php">FAQ</a></li> 
+		<li><a href="contact-us.php">Contacto</a></li>
+		<li><a href=""></a></li>
+		<li class="active"><a href="register.php" target="_parent">Registrarse</a></li>
+		<li><a href="login.php">Iniciar Sesión</a></li>
+	</ul>
+</div>
